5dae3cac074dde8dcaab4d1c9112f212139227d4
yobre25 <ybrenesr@estudiantec.cr>
2023-10-01 15:16:34 -0600

    Add: HPS access to perf

---
+++ hw/Quartus/platform_top.sv
@@ -0,0 +1,57 @@
+module platform_top (
+		input  wire        clk_clk,                          //                       clk.clk
+		output wire [12:0] memory_mem_a,                     //                    memory.mem_a
+		output wire [2:0]  memory_mem_ba,                    //                          .mem_ba
+		output wire        memory_mem_ck,                    //                          .mem_ck
+		output wire        memory_mem_ck_n,                  //                          .mem_ck_n
+		output wire        memory_mem_cke,                   //                          .mem_cke
+		output wire        memory_mem_cs_n,                  //                          .mem_cs_n
+		output wire        memory_mem_ras_n,                 //                          .mem_ras_n
+		output wire        memory_mem_cas_n,                 //                          .mem_cas_n
+		output wire        memory_mem_we_n,                  //                          .mem_we_n
+		output wire        memory_mem_reset_n,               //                          .mem_reset_n
+		inout  wire [7:0]  memory_mem_dq,                    //                          .mem_dq
+		inout  wire        memory_mem_dqs,                   //                          .mem_dqs
+		inout  wire        memory_mem_dqs_n,                 //                          .mem_dqs_n
+		output wire        memory_mem_odt,                   //                          .mem_odt
+		output wire        memory_mem_dm,                    //                          .mem_dm
+		input  wire        memory_oct_rzqin,                 //                          .oct_rzqin
+		output wire        pio_led_0_external_connection_export, // pio_0_external_connection.export
+		input  wire [3:0]  pio_switches_0,
+		output wire [6:0]  display_0, // pio_0_external_connection.export
+		input  wire        reset_reset_n                     //                     reset.reset_n
+);
+	
+	
+	
+	
+	platform plat(
+		.clk_clk(clk_clk),                                
+		.memory_mem_a(memory_mem_a),                            
+		.memory_mem_ba(memory_mem_ba),    
+		.memory_mem_ck(memory_mem_ck),                          
+		.memory_mem_ck_n(memory_mem_ck_n),                      
+		.memory_mem_cke(memory_mem_cke),                        
+		.memory_mem_cs_n(memory_mem_cs_n),                      
+		.memory_mem_ras_n(memory_mem_ras_n),                    
+		.memory_mem_cas_n(memory_mem_cas_n),                    
+		.memory_mem_we_n(memory_mem_we_n),                      
+		.memory_mem_reset_n(memory_mem_reset_n),                
+		.memory_mem_dq(memory_mem_dq),                          
+		.memory_mem_dqs(memory_mem_dqs),                        
+		.memory_mem_dqs_n(memory_mem_dqs_n),           
+		.memory_mem_odt(memory_mem_odt),                    
+		.memory_mem_dm(memory_mem_dm),                    
+		.memory_oct_rzqin(memory_oct_rzqin),
+		.pio_led_0_external_connection_export(pio_led_0_external_connection_export),
+		.pio_switches_0_external_connection_export(pio_switches_0),
+		.pio_num_0_external_connection_export(display_0),
+		.reset_reset_n(reset_reset_n)
+		);
+		
+//	seven_segment_decoder ss_decoder(
+//    .input_bits(pio_num_0),
+//    .segments(display_0)
+//	);
+
+endmodule
---
+++ sw/perifericos.c
@@ -0,0 +1,143 @@
+// #include <stdio.h>
+// #include <stdint.h>
+// #include <fcntl.h>
+// #include <unistd.h>
+// #include <sys/mman.h>
+
+// #define LWHPS2FPGA_Base 0xFF200000  // Address of the GPIO controller in LWHPS2FPGA bridge
+// #define SWITCHES_OFFSET 0x00
+// #define DISPLAY0_OFFSET 0X10
+
+
+// // void seven_segment_decoder(uint32_t num){
+
+
+// // }
+
+
+// int main() {
+//     int fd = open("/dev/mem", O_RDWR|O_SYNC);
+//     if (fd < 0) {
+//         perror("open");
+//         return 1;
+//     }
+
+//     void* virtual_base = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED, fd, LWHPS2FPGA_Base);
+//     if (virtual_base == MAP_FAILED) {
+//         perror("mmap");
+//         return 1;
+//     }
+
+//     // volatile uint32_t* switches_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + SWITCHES_OFFSET);
+//     volatile uint32_t* display_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + DISPLAY0_OFFSET);
+
+//     while (1) {
+//         uint32_t num = 0xa;
+//         uint32_t display_value;
+//             switch(num){
+//         case 0xb:
+//             display_value = (uint32_t)0b0000011;
+//         case 0xc:
+//             display_value = (uint32_t)0b1000110;
+//         case 0xd:
+//             display_value = (uint32_t)0b0100001;
+//         case 0xe:
+//             display_value = (uint32_t)0b0000110;
+//         case 0xf:
+//             display_value = (uint32_t)0b0001110;
+//         default:
+//             display_value = (uint32_t)0b1000000;
+//     }
+//         *display_ptr = display_value;
+//         printf("Valor al que apunta el puntero: %x \n", *display_ptr);
+//         sleep(0.5); 
+//     }
+
+//     munmap(virtual_base, 4096);
+//     close(fd);
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include<unistd.h>
+#include <sys/mman.h>
+
+#define LW_BASE 0xFF200000  // Address of the GPIO controller in LWHPS2FPGA bridge
+#define LED_OFFSET 0x00
+#define SWITCHES_OFFSET 0x10
+#define NUM_OFFSET 0x20
+
+uint32_t num_to_display(int num){
+    uint32_t display_value;
+    if(num == 0)
+        display_value = 0x40;
+    else if(num == 1)
+        display_value = 0x79;
+    else if(num == 2)
+        display_value = 0x24;
+    else if(num == 3)
+        display_value = 0x30;
+    else if(num == 4)
+        display_value = 0x19;
+    else if(num == 5)
+        display_value = 0x12;
+    else if(num == 6)
+        display_value = 0x02;
+    else if(num == 7)
+        display_value = 0x70;
+    else if(num == 8)
+        display_value = 0x77;
+    else if(num == 9)
+        display_value = 0x14;
+    else if(num == 10)
+        display_value = 0x80;
+    else if(num == 11)
+        display_value = 0x03;
+    else if(num == 12)
+        display_value = 0x64;
+    else if(num == 13)
+        display_value = 0x21;
+    else if(num == 14)
+        display_value = 0x06;
+    else if(num == 15)
+        display_value = 0x0d;
+    else
+        display_value = 0x0;
+    return display_value;
+}
+
+int main() {
+    int fd = open("/dev/mem", O_RDWR|O_SYNC);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    void* virtual_base = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED, fd, LW_BASE);
+    if (virtual_base == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    volatile uint32_t* led_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + LED_OFFSET);
+    volatile uint32_t* switches_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + SWITCHES_OFFSET);
+    volatile uint32_t* num_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + NUM_OFFSET);
+
+    while (1) {
+        *led_ptr = 0x01; 
+        // sleep(1); 
+        // *led_ptr = 0x00; 
+        // sleep(1); 
+        int switches_value = *((volatile uint32_t *)switches_ptr);
+        *num_ptr = num_to_display(switches_value);
+        // printf("Valor de los switches: %x \n", switches_value);
+        // printf("Valor de num: %x \n", *((volatile uint32_t *)num_ptr));
+        // printf("---------------------- \n");
+    }
+
+    munmap(virtual_base, 4096);
+    close(fd);
+    return 0;
+}
