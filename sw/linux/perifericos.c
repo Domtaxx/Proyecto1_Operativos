0c92c6537aaae0485caa9efb5e1b48538f80cd8e
yobre25 <ybrenesr@estudiantec.cr>
2023-10-02 08:39:31 -0600

    update: clean code

--- sw/linux/perifericos.c
+++ sw/linux/perifericos.c
@@ -7,7 +7,8 @@
 #define LW_BASE 0xFF200000  // Address of the GPIO controller in LWHPS2FPGA bridge
 #define ENTER_OFFSET 0x00
 #define SWITCHES_OFFSET 0x10
-#define NUM_OFFSET 0x20
+#define EDGE_CAPTURE_OFFSET 0x03
+#define DISPLAY_OFFSET 0x20
 
 uint32_t num_to_display(int num){
     uint32_t display_value;
@@ -62,26 +63,18 @@ int main() {
     }
 
     volatile uint32_t* enter_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + ENTER_OFFSET);
-    // volatile uint32_t* enter_edge_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + ENTER_OFFSET + 16);
-    volatile uint32_t* switches_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + SWITCHES_OFFSET);
-    volatile uint32_t* display_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + NUM_OFFSET);
+    volatile uint32_t* switch_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + SWITCHES_OFFSET);
+    volatile uint32_t* display_ptr = (volatile uint32_t*)((uint8_t*)virtual_base + DISPLAY_OFFSET);
 
     while (1) {
-        sleep(1); 
-        *enter_ptr = 0x01;
-        volatile int switches_value = *((volatile uint32_t *)switches_ptr) & 0xf;
-        *display_ptr = num_to_display(switches_value);
-        sleep(1);
-        *enter_ptr = 0x00;
-        // printf("Valor de enter es: %x \n", *((volatile uint32_t *)(enter_ptr)));
-        printf("Valor del switch: %x \n", switches_value);
-        printf("Valor del edgecaputure: %x \n", *(switches_ptr + 0x03));
-        printf("%x \n", *(switches_ptr + 0x03) >> 4);
+        volatile int switch_value = *((volatile uint32_t *)switch_ptr) & 0xf;
+        *display_ptr = num_to_display(switch_value);
         
-        if((*(switches_ptr + 0x03) >> 4) & 1 == 1) {
+        volatile uint32_t* switch_edgecapture_ptr = switch_ptr + EDGE_CAPTURE_OFFSET;
+        uint32_t edgecapture_value = (*(switch_edgecapture_ptr) >> 4) & 1;
+        if(edgecapture_value == 1) {
             printf("Entró al if\n");
-            *display_ptr = 0x00;
-            *(switches_ptr + 0x03) = 0x00;
+            *switch_edgecapture_ptr = 0x00;
         }
     }
 
